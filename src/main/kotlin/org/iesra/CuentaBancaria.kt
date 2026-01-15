5c138be96420a36e1d34fe3046edd39012c2dc3e
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-01-15 16:59:10 +0100

    Solucionado errores lógica

--- src/main/kotlin/org/iesra/CuentaBancaria.kt
+++ src/main/kotlin/org/iesra/CuentaBancaria.kt
@@ -1,9 +1,11 @@
-class CuentaBancaria(private val titular : String, private val saldo : Double = 0.0){
+package org.iesra
+
+class CuentaBancaria(private val titular : String, private var saldo : Double = 0.0){
     fun ingresar(cantidad : Double){
         if(cantidad <= 0){
             throw IllegalArgumentException("La cantidad a ingresar tiene que ser mayor que 0.")
         }
-        this.saldo += saldo
+        this.saldo += cantidad
         println("Saldo restante : ${this.saldo}")
     }
 
@@ -11,7 +13,7 @@ class CuentaBancaria(private val titular : String, private val saldo : Double = 
         if(cantidad > this.saldo){
             throw IllegalArgumentException("La cantidad a retirar no puede ser mayor que el saldo actual.")
         }
-        this.saldo += cantidad
+        this.saldo -= cantidad
         println("Saldo restante : ${this.saldo}")
     }
 }
