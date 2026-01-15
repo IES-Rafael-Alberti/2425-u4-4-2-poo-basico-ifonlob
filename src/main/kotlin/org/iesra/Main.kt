bcdff0bebd0052c6f4258d91672404356b485ac8
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-01-15 17:18:44 +0100

    Implementado segundo ejercicio

--- src/main/kotlin/org/iesra/Main.kt
+++ src/main/kotlin/org/iesra/Main.kt
@@ -1,6 +1,9 @@
 package org.iesra
 
 fun main(){
+
+    /* EJERCICIO 1 */
+
     val cuenta = CuentaBancaria("Juan")
     cuenta.ingresar(100.0)
     cuenta.retirar(50.0)
@@ -10,5 +13,12 @@ fun main(){
     catch(e : IllegalArgumentException){
         println("La cantidad a retirar no puede ser mayor que el saldo actual.")
     }
+
+    /* EJERCICIO 2 */
+
+    val vehiculo = Vehiculo("Toyota","Hybrid")
+    vehiculo.registrarViaje(100.0)
+    println(vehiculo.detalles())
+
 
 }
