26218e10ad2e4add5fcbeec16cd11b4d4b2a1d04
en3k1x <ifoncubiertal@iesrafaelalberti.es>
2026-01-16 09:11:28 +0100

    Arreglada condición if

--- src/main/kotlin/org/iesra/Estudiante.kt
+++ src/main/kotlin/org/iesra/Estudiante.kt
@@ -3,16 +3,11 @@ package org.iesra
 class Estudiante(private val nombre : String){
     var nota : Double = 0.0
         set(value){
-            if(value > 0.0 && value < 10.0){
+            if(value < 0.0 || value > 10.0){
                 throw IllegalArgumentException("La nota debe estar entre 0 y 10.")
             }
             field = value
         }
-
-
-    fun getNota(): Double {
-        return nota
-    }
 
     override fun toString(): String {
         return "Estudiante: $nombre, Nota=$nota"
